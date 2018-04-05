9b6807f4a050af76f7728b163516abc736b26423
erick <erick@hotmail.ca>
2018-04-05 15:43:03 -0400

    Add Node structure for tree search

---
+++ src/student_player/Node.java
@@ -0,0 +1,36 @@
+package student_player;
+
+import java.util.ArrayList;
+import java.util.List;
+import tablut.TablutBoardState;
+
+public class Node {
+	private TablutBoardState boardState;
+	private Node parent;
+	private List<Node> children;
+	
+	public Node() {
+        super();
+        children = new ArrayList<Node>();
+	}
+	
+	public Node getParent() {
+		return parent;
+	}
+	
+	public List<Node> getChildren() {
+		return children;
+	}
+	
+	public List<Node> addChild(Node node) {
+		children.add(node);
+		return children;
+	}
+	
+	public TablutBoardState getBoardState() {
+		return boardState;
+	}
+	public void setBoardState(TablutBoardState boardState) {
+		this.boardState = boardState;
+	}
+}
