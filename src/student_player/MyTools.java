a4f80b12df38ce287becb5c62e196f95c6d68d6f
erick <erick@hotmail.ca>
2018-04-06 21:15:29 -0400

    Add utility to measure move distance

--- src/student_player/MyTools.java
+++ src/student_player/MyTools.java
@@ -1,7 +1,13 @@
 package student_player;
 
+import tablut.TablutMove;
+
 public class MyTools {
     public static double getSomething() {
         return Math.random();
+    }
+    
+    public static int getMoveLength(TablutMove m) {
+    	return m.getStartPosition().distance(m.getEndPosition());
     }
 }
